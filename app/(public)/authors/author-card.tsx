fde7bc6deef345e8f914339abea05f01cf21be4c
Soheil Hassanjani <soheilhassanjani@gmail.com>
2024-07-08 19:16:01 +0330

    .

---
+++ app/(public)/authors/author-card.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { FC } from "react";
+
+interface AuthorCardProps {
+  id: string;
+  imageSrc?: string;
+  fullName: string;
+}
+
+const AuthorCard: FC<AuthorCardProps> = ({ id, imageSrc, fullName }) => {
+  return (
+    <figure className="relative aspect-square rounded border-4 border-[#0f70b7]">
+      {imageSrc ? (
+        <Image src={imageSrc} alt="author-profile" fill />
+      ) : (
+        <Image
+          src="/images/placeholder-author.jpg"
+          alt="author-profile-placeholder"
+          fill
+        />
+      )}
+
+      <div className="absolute start-0 top-0 h-full w-full bg-[linear-gradient(0deg,rgba(0,0,0,.66)_10%,rgba(255,255,255,0)_100%)]" />
+
+      <figcaption className="absolute bottom-6 start-1/2 flex translate-x-1/2 flex-col items-center px-5 text-center">
+        <h2 className="mb-2 whitespace-nowrap text-lg text-white drop-shadow-lg">
+          {fullName}
+        </h2>
+        <Link
+          href={`/authors/${id}/${fullName}`}
+          className="block w-fit whitespace-nowrap border-2 border-[#0f70b7] px-3 py-1.5 text-white transition hover:bg-[#0f70b7]"
+        >
+          مشاهده رزومه
+        </Link>
+      </figcaption>
+    </figure>
+  );
+};
+
+export default AuthorCard;
--- app/(public)/authors/page.tsx
+++ app/(public)/authors/page.tsx
@@ -1,12 +1,41 @@
-import AuthorsList from "@/components/pages/authors/AuthorsList";
-import React, { Suspense } from "react";
+import React from "react";
+import { HOST_ADDRESS } from "@/configs/baseUrl";
+import AuthorCard from "@/app/(public)/authors/author-card";
 
-const AuthorsPage = () => {
+async function getData() {
+  const res = await fetch(HOST_ADDRESS + "/user/GetAllAuthorList", {
+    method: "POST",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const AuthorsPage = async () => {
+  //
+  const data = await getData();
+  //
   return (
     <div className="container my-4">
-      <Suspense fallback={<></>}>
-        <AuthorsList />
-      </Suspense>
+      <div className="grid grid-cols-12 gap-4">
+        {data?.map((author: any) => (
+          <div
+            key={author.ID}
+            className="col-span-6 sm:col-span-4 lg:col-span-3"
+          >
+            <AuthorCard
+              id={author.ID}
+              fullName={author.FullName}
+              imageSrc={
+                author.ImageUrl
+                  ? "http://masterdba.ir:8080" + author.ImageUrl
+                  : undefined
+              }
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
