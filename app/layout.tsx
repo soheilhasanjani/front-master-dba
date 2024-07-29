7e43b46e869974b8e9e9d0b4d969368d9f5dbc58
Soheil Hassanjani <soheilpcmir@gmail.com>
2024-07-29 21:27:21 +0330

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,33 +3,43 @@ import Providers from "@/components/providers";
 import { IRANYekanX } from "@/theme/fonts";
 import "@/styles/globals.css";
 import {
+  postPanelCustomValueGetLogImage,
   postPanelCustomValueGetMainPageKeyWord,
   postPanelCustomValueGetWebSiteTitle,
 } from "@/apis/panelCustomValueApi";
+import staticFileUrl from "@/utils/staticFileUrl";
 
 export async function generateMetadata(): Promise<Metadata> {
   // fetch data
   // Wait for the promises to resolve
-  const [title, keywords] = await Promise.all([
+  const [title, keywords, logo] = await Promise.all([
     postPanelCustomValueGetWebSiteTitle(),
     postPanelCustomValueGetMainPageKeyWord(),
+    postPanelCustomValueGetLogImage(),
   ]);
 
   return {
     title: title,
     keywords: keywords,
+    description: keywords,
     robots: "index, follow",
     openGraph: {
       title: title,
+      description: keywords,
+      images: staticFileUrl(logo),
+    },
+    twitter: {
+      description: keywords,
+      images: staticFileUrl(logo),
     },
   };
 }
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="fa" dir="rtl">
       <body className={IRANYekanX.variable}>
