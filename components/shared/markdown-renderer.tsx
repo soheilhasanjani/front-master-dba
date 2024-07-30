af26fc1a5159fe64c0b0ff6ba11445e77fd2dfe3
Soheil Hassanjani <soheilhassanjani@gmail.com>
2024-07-30 17:04:35 +0330

    .

--- components/shared/markdown-renderer.tsx
+++ components/shared/markdown-renderer.tsx
@@ -1,69 +1,87 @@
-"use client";
-
 import React, { useEffect } from "react";
-import MarkdownIt from "markdown-it";
-import Prism from "prismjs"; // Prism.js library
-import { IMAGE_BASE_URL } from "@/configs/baseUrl";
-import "prismjs/themes/prism.css"; // Prism.js theme for syntax highlighting
+import Prism from "prismjs";
+import staticFileUrl from "@/utils/staticFileUrl";
+import "prismjs/components/prism-sql";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-markup";
+import "prismjs/components/prism-csharp";
+import "prismjs/themes/prism.css";
 
 interface MarkdownProps {
   content: string;
 }
 
-function filterLanguageClasses(html: string): string {
-  return html.replace(/(<[^>]+)\sclass="([^"]*)"/g, (_, tag, classAttr) => {
-    // Split the class attribute into individual classes
-    const classes = classAttr.split(/\s+/);
-    // Filter classes to keep only those starting with 'language'
-    const filteredClasses = classes.filter((cls: any) =>
-      cls.startsWith("language"),
+function sanitizeClasses(htmlContent: string): string {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlContent, "text/html");
+
+  const elements = doc.querySelectorAll("[class]");
+  elements.forEach((el) => {
+    const classes = el.className.split(" ");
+    const filteredClasses = classes.filter((cls) =>
+      cls.startsWith("language-"),
     );
-    if (filteredClasses.length > 0) {
-      // Join the remaining classes and return the modified tag
-      return `${tag} class="${filteredClasses.join(" ")}"`;
-    } else {
-      // If no classes left, return the tag without the class attribute
-      return tag;
-    }
+    el.className = filteredClasses.join(" ");
   });
+
+  return doc.body.innerHTML;
 }
 
-// Function to prepend base URL to relative image srcs
-const prependBaseUrlToImages = (html: string, baseUrl: string): string => {
-  // Regular expression to find all <img> tags with src attributes
-  const imgTagRegex = /<img\b[^>]*\bsrc="([^"]*)"/gi;
+function updateImageSources(htmlContent: string): string {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlContent, "text/html");
 
-  // Replace each relative src with the base URL prepended
-  return html.replace(imgTagRegex, (match, src: string) => {
-    // If the src is not an absolute URL, prepend the base URL
-    if (src && !src.startsWith("http")) {
-      return match.replace(src, `${baseUrl}/${src}`);
+  const images = doc.querySelectorAll("img");
+  images.forEach((img) => {
+    if (!img.src.startsWith("http://") && !img.src.startsWith("https://")) {
+      img.src = staticFileUrl(img.src);
     }
-    return match; // return the original match if src is already absolute
   });
+
+  return doc.body.innerHTML;
+}
+
+const enhanceCodeBlocks = (htmlContent: string): string => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlContent, "text/html");
+
+  const preElements = doc.querySelectorAll("pre");
+  preElements.forEach((pre) => {
+    const code = pre.querySelector("code");
+    if (code) {
+      const languageClass = Array.from(pre.classList).find((cls) =>
+        cls.startsWith("language-"),
+      );
+      if (languageClass) {
+        code.classList.add(languageClass);
+      }
+    }
+  });
+
+  return doc.body.innerHTML;
 };
 
-const md = new MarkdownIt({
-  html: true,
-  linkify: true,
-  typographer: true,
-});
-
 const MarkdownRenderer: React.FC<MarkdownProps> = ({ content }) => {
-  //
-  const renderMarkdown = (markdownText: string) => {
-    return md.render(
-      filterLanguageClasses(
-        prependBaseUrlToImages(markdownText, IMAGE_BASE_URL ?? ""),
-      ),
-    );
-  };
-  //
+  const processedContent = content
+    ? updateImageSources(sanitizeClasses(enhanceCodeBlocks(content)))
+    : "";
+
   useEffect(() => {
-    Prism.highlightAll();
-  }, [content]);
-  //
-  return <div dangerouslySetInnerHTML={{ __html: renderMarkdown(content) }} />;
+    // Set a slight delay to ensure DOM updates are complete
+    const timeoutId = setTimeout(() => {
+      const codeElements = document.querySelectorAll("pre code");
+      codeElements.forEach((block) => {
+        Prism.highlightElement(block);
+      });
+    }, 1000); // Adjust the delay if necessary
+
+    // Clean up timeout on component unmount
+    return () => clearTimeout(timeoutId);
+  }, [processedContent]);
+
+  return <div dangerouslySetInnerHTML={{ __html: processedContent }} />;
 };
 
 export default MarkdownRenderer;
